4e38e2afeedb920c42afbdcd4cf99abdd25ab05b
Albert <me@albertapsit.com>
2018-09-20 17:13:21 +0300

    initial submit

---
+++ Customer.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace testweb
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        
+        [Required, StringLength(10)]
+        public string Name { get; set; }
+
+
+    }
+}
